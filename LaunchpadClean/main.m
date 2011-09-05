c9491c15eed1d13cf7956d36438df33b34b078bc
Alex Nichol <alexnichol@comcast.net>
2011-09-04 21:17:08 -0400

    Initial Commit

---
+++ LaunchpadClean/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  LaunchpadClean
+//
+//  Created by Alex Nichol on 9/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
